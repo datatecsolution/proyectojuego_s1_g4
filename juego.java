c0bb425389ed0894248b81c44c63fd401e70b242
Anelisse <anelisseportillo1@gmail.com>
2025-07-22 11:07:36 -0600

    agregue clase jugador

---
+++ juego.java
@@ -0,0 +1,20 @@
+public class juego {
+    private String nombre;
+    private int salud;
+    private int fuerza;
+    {
+
+    }
+
+    public void atacar() {
+
+    }
+
+    public void recibirDanio() {
+
+    }
+
+    public void estaVivo() {
+
+    }
+}
