7c246dfda7aaf2651dbd956aaad7a9c7066b7012
unknown <maholyr1215@gmail.com>
2025-08-19 10:26:52 -0600

    Combate

--- src/main/java/net/perlasjp/juego/Combate.java
+++ src/main/java/net/perlasjp/juego/Combate.java
@@ -3,4 +3,9 @@ package net.perlasjp.juego;
 public class Combate{
     private Jugador jugador;
     private Enemigo enemigo;
+
+
+    public void iniciar(jugador:Jugador,enemigo:Enemigo){
+
+    }
 }
