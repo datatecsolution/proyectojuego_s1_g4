59ad5492a495d16043abb320098fd41bbc2e5c87
Anelisse <anelisseportillo1@gmail.com>
2025-08-20 08:58:32 -0600

    cree clase

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -1,5 +1,32 @@
 package net.perlasjp.juego;
 
 public class Jugador {
+    private String nombre;
+    private int salud;
+    private int fuerza;
+
+    public int atacar() {
+
+    }
+
+    public void recibirDanio() {
+
+    }
+
+    public boolean estaVivo() {
+
+    }
+
+    public Jugador(String nombre) {
+
+    }
+
+    public String getSalud() {
+
+    }
+
+    public void mostrarEstado() {
+
+    }
 
 }
