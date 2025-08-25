d10797230f3aad6f0b6773711690e36c36ab7b2d
Moises Sequeira <moisessequeiracontact@gmail.com>
2025-08-25 10:28:41 -0600

    agregue clase

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -1,5 +1,33 @@
 package net.perlasjp.juego;
 
-public class Enemigo {
+public class Enemigo{
+
+private String nombre;
+private int salud;
+private int fuerza;
+
+public int atacar(){
+return 0;
+}
+public void recibirDanio(int danio){
+
 
 }
+public boolean estarVivo(){
+    return false;
+
+}
+
+public Enemigo (int String ){
+
+}
+
+public String getNombre(){
+
+}
+public int getSalud(){
+    return 0; 
+}
+}
+
+
